64da7c8a9ccf423644e4a2c5958a6a02d02d52be
John-Philip Johansson <john-philip.johansson@episerver.com>
2016-10-13 14:35:46 +0200

    Send notifications for tweeted and retweeted content
    This is a working example, although the articles aren't online so no
    tweets will be found. Use the commented out code in GetTweets to try it
    out. The notifications are throttled with a simple "at least twice as
    many as last the notification".

---
+++ src/Business/Twitter/TwitterNotificationFormatter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using EPiServer.Notification;
+using EPiServer.ServiceLocation;
+
+namespace Ascend2016.Business.Twitter
+{
+    /// <summary>
+    /// Handling formation of notifications and instant user notifications.
+    /// </summary>
+    [ServiceConfiguration(typeof(IUserNotificationFormatter))]
+    [ServiceConfiguration(typeof(INotificationFormatter))]
+    public class TwitterNotificationFormatter : IUserNotificationFormatter, INotificationFormatter
+    {
+        public const string ChannelName = "epi-ascend-2016-twitter";
+
+        /// <summary>
+        /// Gets the list of channels supported by this formatter
+        /// </summary>
+        public IEnumerable<string> SupportedChannelNames => new[] { ChannelName };
+
+        /// <summary>
+        /// Gets the name of the formatter
+        /// </summary>
+        public string FormatterName => "Epi-DefaultNotificationFormatter";
+
+        public IEnumerable<FormatterNotificationMessage> FormatMessages(IEnumerable<FormatterNotificationMessage> notifications, string recipient, NotificationFormat format, string channelName)
+        {
+            return notifications;
+        }
+
+        public UserNotificationMessage FormatUserMessage(UserNotificationMessage notification)
+        {
+            return notification;
+        }
+    }
+}
--- src/Business/Twitter/TwitterSearchJob.cs
+++ src/Business/Twitter/TwitterSearchJob.cs
@@ -1,20 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using EPiServer;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
+using EPiServer.Notification;
 using EPiServer.PlugIn;
 using EPiServer.Scheduler;
+using EPiServer.ServiceLocation;
 using EPiServer.Web.Routing;
 using Tweetinvi;
+using Tweetinvi.Core.Interfaces;
 
 namespace Ascend2016.Business.Twitter
 {
     [ScheduledPlugIn(DisplayName = "Twitter Search", IntervalLength = 10, IntervalType = ScheduledIntervalType.Seconds)]
     public class TwitterSearchJob : ScheduledJobBase
     {
+        private static Dictionary<string, IEnumerable<ITweet>> twitterCache = new Dictionary<string, IEnumerable<ITweet>>();
+
+        private Injected<INotifier> _notifier;
         private bool _stopSignaled;
 
         public TwitterSearchJob()
@@ -55,42 +63,94 @@ namespace Ascend2016.Business.Twitter
             }
 
             //Add implementation
-            var page = GetLatestPublishedContent(DateTime.Now.AddDays(-120).ToString(CultureInfo.InvariantCulture));
-            if (page == null)
-                return "Fail...";
-
-            var url = ExternalUrl(page.ContentLink, CultureInfo.CurrentCulture);
-
-            var tweet = GetTweet(url);
-            return tweet ?? $"No tweet found with {url}";
-        }
-
-        private string GetTweet(string url)
-        {
-            //var tweet = Tweet.GetTweet(780929654924378112);
-            //var tweet = Search.SearchTweets("https://medium.com/@shemag8/fuck-you-startup-world-ab6cc72fad0e");
-            var tweet = Search.SearchTweets(url);
-
-            return tweet.FirstOrDefault()?.FullText;
-        }
-
-        private PageData GetLatestPublishedContent(string days)
-        {
-            var criterias = new PropertyCriteriaCollection();
-
-            var criteria = new PropertyCriteria
+            const int numDaysBacklog = 120;
+            var pages = GetLatestPublishedContent(DateTime.Now.AddDays(-numDaysBacklog).ToString(CultureInfo.InvariantCulture)).ToArray();
+            if (!pages.Any())
             {
-                Condition = EPiServer.Filters.CompareCondition.GreaterThan,
-                Name = "PageChanged",
-                Type = PropertyDataType.Date,
-                Value = days,
-                Required = true
+                return $"No pages published in the last {numDaysBacklog} days.";
+            }
+
+            var totalShareCount = 0;
+            var notificationsCount = 0;
+
+            foreach (var page in pages)
+            {
+                var url = ExternalUrl(page.ContentLink, CultureInfo.CurrentCulture);
+                var tweets = GetTweets(url).ToArray();
+
+                if (!tweets.Any())
+                {
+                    continue;
+                }
+
+                // Cache handling
+                var cachedTweets = twitterCache.ContainsKey(url) ? twitterCache[url].ToArray() : new ITweet[0];
+                var union = tweets
+                    .Union(cachedTweets) // ITweet doesn't support comparison so we have to filter out duplicates ourselves for now.
+                    .GroupBy(tweet => tweet.Id)
+                    .Select(group => group.First())
+                    .ToArray();
+                var currentShareCount = CountShares(union);
+
+                totalShareCount += currentShareCount;
+                twitterCache[url] = union;
+
+                // Simple notification rule. There should be at least twice as many tweets as the last notification.
+                var lastShareCount = CountShares(cachedTweets);
+                if (currentShareCount <= lastShareCount*2)
+                {
+                    continue;
+                }
+
+                // Send notification.
+                var recipients = new[] { new NotificationUser(page.ChangedBy) };
+                CreateNotificationMessage("jojoh", recipients, page, currentShareCount).Wait();
+                notificationsCount++;
+            }
+
+            return $"Found {pages.Length} pages that were tweeted {totalShareCount} times. Sent {notificationsCount} notifications.";
+        }
+
+        private static int CountShares(ITweet[] tweets)
+        {
+            return tweets.Length + tweets.Sum(x => x.RetweetCount);
+        }
+
+        private IEnumerable<ITweet> GetTweets(string url)
+        {
+            //var tweets = new [] { Tweet.GetTweet(780929654924378112) };
+            //var tweets = Search.SearchTweets("https://medium.com/@shemag8/fuck-you-startup-world-ab6cc72fad0e");
+            var tweets = Search.SearchTweets(url);
+
+            return tweets;
+        }
+
+        private IEnumerable<PageData> GetLatestPublishedContent(string days)
+        {
+            var criterias = new PropertyCriteriaCollection
+            {
+                new PropertyCriteria
+                {
+                    Condition = EPiServer.Filters.CompareCondition.GreaterThan,
+                    Name = "PageChanged",
+                    Type = PropertyDataType.Date,
+                    Value = days,
+                    Required = true
+                },
+                //new PropertyCriteria
+                //{
+                //    Condition = EPiServer.Filters.CompareCondition.NotEqual,
+                //    Name = "ChangedBy",
+                //    Type = PropertyDataType.String,
+                //    Value = string.Empty,
+                //    Required = true
+                //}
             };
 
-            criterias.Add(criteria);
 
-            var newsPageItems = DataFactory.Instance.FindPagesWithCriteria(PageReference.StartPage, criterias);
-            return newsPageItems.FirstOrDefault();
+            var newsPageItems = DataFactory.Instance.FindPagesWithCriteria(PageReference.StartPage, criterias)
+                .Where(x => !string.IsNullOrEmpty(x.ChangedBy)); // Can't use PropertyCriteria for this? See my attempt above.
+            return newsPageItems;
         }
 
         private static string ExternalUrl(ContentReference contentLink, CultureInfo language)
@@ -112,6 +172,29 @@ namespace Ascend2016.Business.Twitter
             }
 
             return new Uri(HttpContext.Current.Request.Url, uri).ToString();
+        }
+
+        /// <summary>
+        /// Creates and posts the notification message
+        /// </summary>
+        /// <param name="sender">Author of the message</param>
+        /// <param name="recipients">Recipients of the message</param>
+        /// <param name="page">The page shared on Twitter</param>
+        /// <param name="shareCount">Number of tweets and retweets for <see cref="page"/>page</param>
+        /// <returns></returns>
+        private async Task CreateNotificationMessage(string sender, IEnumerable<INotificationUser> recipients, PageData page, int shareCount)
+        {
+            var notificationMessage = new NotificationMessage
+            {
+                ChannelName = TwitterNotificationFormatter.ChannelName,
+                Subject = $@"Your article ""{page.Name}"" is going viral!",
+                TypeName = "Tweet",
+                Category = new Uri("twitter://myhost/"),
+                Sender = new NotificationUser(sender),
+                Recipients = recipients,
+                Content = $"Your article has {shareCount} tweets and retweets!"
+            };
+            await _notifier.Service.PostNotificationAsync(notificationMessage).ConfigureAwait(false);
         }
     }
 }
